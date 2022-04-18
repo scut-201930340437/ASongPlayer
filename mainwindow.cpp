d477805b75d37610b91de9f23385ba0b7d285842
scut-201930340437 <2478987785@qq.com>
2022-04-18 11:51:34 +0800

    2nd-commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,12 +5,24 @@
 #include <QMediaPlayer>
 #include <QAudioOutput>
 #include <QVideoWidget>
+#include <QDebug>
+#include <QMediaMetaData>
+#include <QTimer>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     player=new QMediaPlayer;
+
+    // 绑定mediaplayer信号事件，如获取总时长
+    connect(player, SIGNAL(metaDataAvailableChanged(bool)), this, SLOT(OnMetaDataAvailableChanged(bool)));
+    connect(player, SIGNAL(durationChanged(qint64)), this, SLOT(onDurationChanged(qint64)));
+    connect(player, SIGNAL(stateChanged(QMediaPlayer::State)), this, SLOT(onOriginalStateChanged(QMediaPlayer::State)));
+
+
+
+
     audioOutput=new QAudioOutput;
     videoWidget=new QVideoWidget;
 
@@ -32,7 +44,26 @@ MainWindow::MainWindow(QWidget *parent)
 
     delete palette;
 
+    // 设置音量控制块初始参数
+    ui->volume_ctrl->setMaximum(100);
+    ui->volume_ctrl->setMinimum(0);
+    ui->volume_ctrl->setSingleStep(2);
+    ui->volume_ctrl->setValue(20);
+    ui->volume_ctrl->setTracking(true);
+    // 设置进度条初始参数
 
+    ui->position_ctrl->setMinimum(0);
+    ui->position_ctrl->setValue(0);
+    ui->position_ctrl->setTracking(false);
+
+    // 设置初始音量为20%
+    audioOutput->setVolume(0.2);
+
+    // 进度条定时器
+    position_timer=new QTimer(this);
+    connect(position_timer,&QTimer::timeout,[=](){
+        this->ui->position_ctrl->setValue(int(player->position()/1000));
+    });
 
 }
 
@@ -41,7 +72,14 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+// 获取总时长的响应事件
+void MainWindow::onDurationChanged(qint64 _duration)
+{
+    duration=_duration;
+    ui->position_ctrl->setMaximum(duration/1000);
+}
 
+// 打开文件
 void MainWindow::on_action_openFile_triggered()
 {
     // 定义文件对话框类
@@ -53,35 +91,131 @@ void MainWindow::on_action_openFile_triggered()
     if(!filename.isEmpty()){
         player->setSource(QUrl::fromLocalFile(filename));
         videoWidget->show();
+        ui->position_ctrl->setValue(0);
+
         player->play();
+
+        position_timer->start(1000);
+
         isPlaying=true;
+        existFile=true;
         this->ui->play_button->setText("暂停");
+
+
+
     }else{
         qDebug("打开文件失败！");
     }
+
+
 }
 
-
+// 播放按钮
 void MainWindow::on_play_button_clicked()
 {
-    if(isPlaying){
-        player->pause();
-        isPlaying=false;
-        this->ui->play_button->setText("播放");
-    }else{
-        isPlaying=true;
-        player->play();
-        this->ui->play_button->setText("暂停");
+    if(!existFile){
+        // 定义文件对话框类
+        filename = QFileDialog::getOpenFileName(this,
+                                                            tr("选择音视频文件"),
+                                                            tr("."),
+                                                            tr(
+                                                                "音频文件(*.mp3);;视频文件(*.mp4 *.flv *.avi);;所有文件(*.*)"));
+        if(!filename.isEmpty()){
+            player->setSource(QUrl::fromLocalFile(filename));
+            videoWidget->show();
+
+            ui->position_ctrl->setValue(0);
+
+//            player->setPlaybackRate(8.0);
+
+            player->play();
+
+            position_timer->start(1000);
+
+            isPlaying=true;
+            existFile=true;
+            this->ui->play_button->setText("暂停");
+
+
+
+        }else{
+            qDebug("打开文件失败！");
+        }
+
     }
+    else{
+        if(isPlaying){
+            player->pause();
+            isPlaying=false;
+            this->ui->play_button->setText("播放");
+        }else{
+            player->play();
+            isPlaying=true;
+            this->ui->play_button->setText("暂停");
+        }
+    }
+
+
 }
 
-
+// 停止按钮
 void MainWindow::on_stop_button_clicked()
 {
 
         player->stop();
+//        position_timer->stop();
         isPlaying=false;
+        existFile=false;
         this->ui->play_button->setText("播放");
 
+}
+
+// 静音按钮
+void MainWindow::on_mute_button_clicked()
+{
+    if(isMute){
+        audioOutput->setMuted(false);
+        audioOutput->setVolume((float)preVolume/100);
+        isMute=false;
+        this->ui->mute_button->setText("静音");
+    }else{
+        audioOutput->setMuted(true);
+        isMute=true;
+        this->ui->mute_button->setText("解除静音");
+        preVolume=this->ui->volume_ctrl->value();
+    }
+}
+
+// 音量控制块
+void MainWindow::on_volume_ctrl_valueChanged(int value)
+{
+    audioOutput->setVolume((float)value/100);
+    if(isMute&&value>0){
+        audioOutput->setMuted(false);
+        isMute=false;
+        this->ui->mute_button->setText("静音");
+    }
+    if(value==0){
+        audioOutput->setMuted(true);
+        isMute=true;
+        this->ui->mute_button->setText("解除静音");
+    }
+}
+
+// 拖动进度条的响应事件
+void MainWindow::on_position_ctrl_valueChanged(int value)
+{
+    if(positionCtrlPressed&&existFile){
+        positionCtrlPressed=false;
+        player->setPosition(duration*((float)value/this->ui->position_ctrl->maximum()));
+        position_timer->start(1000);
+    }
+}
+
+
+void MainWindow::on_position_ctrl_sliderPressed()
+{
+    positionCtrlPressed=true;
+    position_timer->stop();
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -15,8 +15,7 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
-
+    void getDuration();
 
 
 private slots:
@@ -26,12 +25,33 @@ private slots:
 
     void on_stop_button_clicked();
 
+    void on_mute_button_clicked();
+
+
+    void on_volume_ctrl_valueChanged(int value);
+
+    void on_position_ctrl_valueChanged(int value);
+
+    void onDurationChanged(qint64 _duration);
+
+    void on_position_ctrl_sliderPressed();
+
+
+
 private:
     Ui::MainWindow *ui;
     QString filename="";
     QMediaPlayer *player=NULL;
     QAudioOutput *audioOutput=NULL;
     QVideoWidget *videoWidget=NULL;
+    QTimer *position_timer=NULL;
     bool isPlaying=false;
+    bool existFile=false;
+    bool isMute=false;
+
+    bool positionCtrlPressed=false;
+
+    int preVolume=20;
+    qint64 duration=0;
 };
 #endif // MAINWINDOW_H
